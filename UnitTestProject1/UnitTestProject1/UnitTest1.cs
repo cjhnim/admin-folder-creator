3a072b52c8020fe6f6742648fd354ce790c10428
choi jung hyun <cjhnim@gmail.com>
2019-06-19 15:00:45 +0900

    initial commit

---
+++ UnitTestProject1/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Security.AccessControl;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            FileSystemAccessRule administratorRule = new FileSystemAccessRule("Administrators", FileSystemRights.FullControl, InheritanceFlags.ContainerInherit | InheritanceFlags.ObjectInherit, PropagationFlags.None, AccessControlType.Allow);
+
+            FileSystemAccessRule everyoneRule = new FileSystemAccessRule("EveryOne", FileSystemRights.CreateDirectories | FileSystemRights.CreateFiles, AccessControlType.Allow);
+
+            DirectorySecurity dirSec = new DirectorySecurity();
+            dirSec.AddAccessRule(everyoneRule);
+            dirSec.AddAccessRule(administratorRule);
+
+            DirectoryInfo info = Directory.CreateDirectory(@"C:\GuestTemp", dirSec);
+
+            Assert.IsTrue(info.Exists);
+        }
+    }
+}
